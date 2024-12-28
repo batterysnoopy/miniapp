290cc90fb8e436a6e92fd9dd56e2ad1cdbe89970
ganchi1989 <ganchizhang1989@gmail.com>
2024-12-28 22:00:13 +0000

    changes

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
+  //swcMinify: true,
   output: 'export'
   /* config options here */
 };
