8fbd3b93e222309cf20067fa09858d488a1ac54a
ganchi1989 <ganchizhang1989@gmail.com>
2024-12-28 22:21:46 +0000

    update

--- app/page.tsx
+++ app/page.tsx
@@ -18,6 +18,7 @@ export default function Home() {
   const [userData, setUserData] = useState<UserData | null>(null)
 
   useEffect(() => {
+    console.log(window.location.href); // Client-side only
     if (WebApp.initDataUnsafe.user) {
       setUserData(WebApp.initDataUnsafe.user as UserData)
     }
